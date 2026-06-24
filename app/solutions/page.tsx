f4609e2eea1f1099693c2df859cabe7d5af3a444
Tanimowo David <davidtanimowo01@gmail.com>
2026-06-24 12:13:14 +0100

    ready to begin solutions page

--- app/solutions/page.tsx
+++ app/solutions/page.tsx
@@ -1,11 +1,1 @@
-// import { SolutionsHero } from "./_components/SolutionsHero";
-// import { SolutionsGrid } from "./_components/SolutionsGrid";
 
-// export default function SolutionsPage() {
-//   return (
-//     <div className="flex flex-col">
-//       <SolutionsHero />
-//       <SolutionsGrid />
-//     </div>
-//   );
-// }
